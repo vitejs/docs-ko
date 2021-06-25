16de97504e6189a18d772d5c95b669b11d3e6ec7
shj <kan02134@gmail.com>
2021-06-26 01:50:37 +0900

    build: add markdown extension scripts

---
+++ .vitepress/render-permalink/index.js
@@ -0,0 +1,47 @@
+const position = {
+  false: 'push',
+  true: 'unshift',
+};
+
+module.exports = (slug, options, state, permalink) => {
+  try {
+    const tokens = state.tokens;
+    const token = tokens[permalink];
+    const title = tokens[permalink + 1]
+      .children
+      .filter(token => token.type === 'text' || token.typoe === 'code_inline')
+      .reduce((acc, token) => acc + token.content, '');
+    const match = /^.+(\s*\{#([a-z0-9\-_]+?)\}\s*)$/.exec(title);
+
+    slug = match ? match[2] : slug;
+    token.attrSet('id', slug);
+
+    const space = () => Object.assign(
+      new state.Token('text', '', 0),
+      { content: '' },
+    );
+
+    const linkTokens = [
+      Object.assign(new state.Token('link_open', 'a', 1), {
+        attrs: [
+          ...(options.permalinkClass ? [['class', options.permalinkClass]] : []),
+          ['href', options.permalinkHref(slug, state)],
+          ...Object.entries(options.permalinkAttrs(slug, state)),
+        ],
+      }),
+      Object.assign(
+        new state.Token('html_block', '', 0),
+        { content: options.permalinkSymbol },
+      ),
+      new state.Token('link_close', 'a', -1),
+    ];
+
+    if (options.permalinkSpace) {
+      linkTokens[position[!options.permalinkBefore]](space());
+    }
+
+    state.tokens[permalink + 1].children[position[options.permalinkBefore]](...linkTokens);
+  } catch (err) {
+    console.log(err);
+  }
+}
