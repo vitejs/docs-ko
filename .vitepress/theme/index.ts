232b256a311d215855bf24ea1f374d3ba46183f1
Gumball12 <to@shj.rip>
2022-06-01 21:24:03 +0900

    fix: fix typo

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,14 +2,14 @@ import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import HomeSponsors from './components/HomeSponsors.vue'
-import ASideSponsors from './components/ASideSponsors.vue'
+import AsideSponsors from './components/AsideSponsors.vue'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
       'home-features-after': () => h(HomeSponsors),
-      'aside-ads-before': () => h(ASideSponsors)
+      'aside-ads-before': () => h(AsideSponsors)
     })
   }
 }
