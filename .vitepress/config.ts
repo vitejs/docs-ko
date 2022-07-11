3f472335399ea3859f1989275b71d7a4aa0ababa
Gumball12 <to@shj.rip>
2022-07-11 23:54:08 +0900

    docs(sync): resolve #304

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -61,7 +61,8 @@ export default defineConfig({
       items: [
         { text: 'English', link: 'https://vitejs.dev' },
         { text: '简体中文', link: 'https://cn.vitejs.dev' },
-        { text: '日本語', link: 'https://ja.vitejs.dev' }
+        { text: '日本語', link: 'https://ja.vitejs.dev' },
+        { text: 'Español', link: 'https://es.vitejs.dev' }
       ]
     },
 
