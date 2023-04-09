eee381d0d7293a226e7d200e92f3bb07aab6e301
Gumball12 <to@shj.rip>
2023-04-10 02:04:14 +0900

    docs(sync): resolve #527

--- .vitepress/theme/components/HomeSponsors.vue
+++ .vitepress/theme/components/HomeSponsors.vue
@@ -8,9 +8,60 @@ const { data } = useSponsor()
 <template>
   <VPHomeSponsors
     v-if="data"
-    message="Vite는 멋진 스폰서들 덕분에 누구나 사용 가능한 오픈 소스가 될 수 있었습니다."
-    action-text="후원하기"
-    action-link="https://github.com/sponsors/yyx990803"
+    message="훌륭한 후원자들 덕분에 Vite는 누구나 사용 가능한 무료 오픈 소스가 될 수 있었습니다."
     :data="data"
   />
+  <div class="action">
+    <a
+      class="sponsor"
+      href="https://github.com/sponsors/vitejs"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Vite 후원하기
+    </a>
+    <a
+      class="sponsor"
+      href="https://github.com/sponsors/yyx990803"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Evan You 후원하기
+    </a>
+  </div>
 </template>
+
+<style scoped>
+.action {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  padding-top: 4rem;
+}
+
+.sponsor {
+  /* .VPButton */
+  display: inline-block;
+  border: 1px solid transparent;
+  text-align: center;
+  font-weight: 600;
+  white-space: nowrap;
+  transition: color 0.25s, border-color 0.25s, background-color 0.25s;
+  /* .VPButton.medium */
+  border-radius: 20px;
+  padding: 0 20px;
+  line-height: 38px;
+  font-size: 14px;
+  /* .VPButton.sponsor */
+  border-color: var(--vp-button-sponsor-border);
+  color: var(--vp-button-sponsor-text);
+  background-color: var(--vp-button-sponsor-bg);
+}
+
+.sponsor:hover {
+  /* .VPButton.sponsor:hover */
+  border-color: var(--vp-button-sponsor-hover-border);
+  color: var(--vp-button-sponsor-hover-text);
+  background-color: var(--vp-button-sponsor-hover-bg);
+}
+</style>
