06e1667c1550f8d6bfeb72d7347d0a2b4eaa2bb3
Gumball12 <to@shj.rip>
2024-12-02 00:10:35 +0900

    docs: refactor environment api docs
    resolved #975

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -328,12 +328,33 @@ export default defineConfig({
               link: '/guide/api-javascript',
             },
             {
-              text: 'Environment API',
+              text: 'Vite 설정 레퍼런스',
+              link: '/config/',
+            },
+          ],
+        },
+        {
+          text: 'Environment API',
+          items: [
+            {
+              text: 'Introduction',
               link: '/guide/api-environment',
             },
             {
-              text: 'Vite 설정 레퍼런스',
-              link: '/config/',
+              text: 'Environment instances',
+              link: '/guide/api-environment-instances',
+            },
+            {
+              text: 'Plugins',
+              link: '/guide/api-environment-plugins',
+            },
+            {
+              text: 'Frameworks',
+              link: '/guide/api-environment-frameworks',
+            },
+            {
+              text: 'Runtimes',
+              link: '/guide/api-environment-runtimes',
             },
           ],
         },
